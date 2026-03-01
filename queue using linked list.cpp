ebb050942027707ef68b24b941df543e939e5c65
Joshi bezavada <joshibezavada55@gmail.com>
2026-03-01 11:15:42 +0530

    Add files via upload

---
+++ queue using linked list.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* front = NULL;
+struct Node* rear = NULL;
+
+void enqueue(int x) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->next = NULL;
+    if(rear == NULL) {
+        front = rear = newNode;
+        return;
+    }
+    rear->next = newNode;
+    rear = newNode;
+}
+
+void dequeue() {
+    if(front == NULL) {
+        printf("Queue Underflow\n");
+        return;
+    }
+    struct Node* temp = front;
+    printf("Dequeued: %d\n", temp->data);
+    front = front->next;
+    if(front == NULL) rear = NULL;
+    free(temp);
+}
+
+void display() {
+    struct Node* temp = front;
+    printf("Queue elements: ");
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    display();
+
+    dequeue();
+    display();
+
+    return 0;
+}

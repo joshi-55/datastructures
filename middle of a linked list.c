bf16c819511f28d0b981d958d19b5d3992e1317c
Joshi bezavada <joshibezavada55@gmail.com>
2026-05-12 09:44:11 +0530

    Implement Node class and start getMiddle method.c
    Add Node class and initial structure for Solution class.

---
+++ middle of a linked list.c
@@ -0,0 +1,17 @@
+class Node {
+    public int data;
+    public Node next;
+
+    public Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public int getMiddle(Node head) {
+        // code here
+        
+    }
+}
